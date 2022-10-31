717174e345e317ef286aa6a5ec6647ce08cac0d8
Adam Kabak <adamkabak@yahoo.com>
2022-10-31 13:52:50 -0400

    commit tree fe32d5caa9fb9ab072dad25ecafd44100bcf0257

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require 'user.rb'
+require 'pry'
+require 'awesome_print'
 
+class Student < User
+    def initialize 
+        @knowledge = []
+    end
+
+    def learn(str) 
+        @knowledge << str
+    end
+
+    def knowledge 
+        @knowledge
+    end
 end
---
+++ lib/user.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'awesome_print'
+
+class User 
+    attr_accessor :first_name, :last_name
+
+    
+end
